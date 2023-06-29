1d3713a6f55c8218c928954ecd6ba3648c226f68
kvorozhcov <git.user.akimbosan18@gmail.com>
2023-06-29 19:40:16 +0300

    Exception Handling

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int i = 1;
+        boolean fileExists;
+        boolean isDirectory;
         while (true) {
             System.out.println("Укажите путь");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
-            boolean fileExists = file.exists();
-            boolean isDirectory = file.isDirectory();
+            fileExists = file.exists();
+            isDirectory = file.isDirectory();
             if (!fileExists) {
                 System.out.println("Указанный файл не существует");
                 continue;
@@ -20,8 +24,32 @@ public class Main {
             };
             System.out.println("Путь указан верно; Это файл номер " + i);
             i++;
+            try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            int j = 0;
+            int max = 0, min = 2147483647;
+            while ((line = reader.readLine()) != null) {
+                int length = line.length();
+                if (length > 1024) {
+                    throw new RuntimeException ("Длина строки более 1024 символов");
+                };
+                if (length > max) {
+                    max = length;
+                };
+                if (length < min) {
+                    min = length;
+                }
+                j++;
+            }
+                System.out.println("Общее кол-во строк в файле: " + j);
+                System.out.println("Длина самой длинной строки в файле: " + max);
+                System.out.println("Длина самой короткой строки в файле: " + min);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
 
     }
 }
-
